fa2f3f22045d1368f8e68eab749874cd09fc2d0a
Amrit Gautam <gautamamrit24@gmail.com>
2017-01-08 23:53:07 +0200

    Client side Setup - 1

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var faker = require('faker');
+
 var app = express();
+
 app.get('/',function(req, res, next){
     res.send("App is UP and RUNNING");
+    next();
+});
+app.get('/random-user',function(req, res, next){
+    var user = faker.helpers.userCard();
+    user.avatar = faker.image.avatar();
+    res.json(user);
     next();
 });
 app.listen(3000,function(){
